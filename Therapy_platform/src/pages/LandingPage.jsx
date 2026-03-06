3cbbf0851b83ccea412cbcac084a123e8494d95a
Akong Rodney <rodneyakong@gmail.com>
2026-03-06 13:58:29 +0100

    finish designing the landing page

--- Therapy_platform/src/pages/LandingPage.jsx
+++ Therapy_platform/src/pages/LandingPage.jsx
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
 
   return (
-    <div className="bg-gradient-to-b from-green-50 to-gray-50 min-h-screen">
+    <div className="bg-gray-800 min-h-screen">
 
       <Navbar />
 
-      <section className="text-center py-32 px-6">
+      <section className="text-center py-32 px-6 bg-gray-800">
 
-        <h1 className="text-5xl font-bold max-w-3xl mx-auto">
+        <h1 className="text-5xl font-bold max-w-3xl mx-auto text-green-300">
           You Don't Have To Go Through It Alone
         </h1>
 
-        <p className="mt-6 text-gray-600 text-lg">
+        <p className="mt-6 text-gray-300 text-lg text-3xl font-bold" >
           Secure online therapy anywhere in Nigeria
         </p>
 
@@ -22,7 +22,7 @@ export default function LandingPage() {
 
           <Link
             to="/signup"
-            className="bg-green-600 text-white px-6 py-3 rounded-xl"
+            className="bg-green-500 text-white px-6 py-3 rounded-xl"
           >
             Start Therapy
           </Link>
